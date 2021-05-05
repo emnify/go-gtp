d258e63e6be1db239a0c7a8de2cd533d9d9590b9
Falko Axmann <falko.axmann@emnify.com>
2021-05-05 20:11:49 +0200

    add more AUTs

--- gtpv1/ie/uli_test.go
+++ gtpv1/ie/uli_test.go
@@ -33,3 +33,55 @@ func TestUserLocationInformationWithCGI(t *testing.T) {
 		}
 	})
 }
+
+func TestUserLocationInformationWithRAI(t *testing.T) {
+	t.Run("Test UserLocationInformation with RAI", func(t *testing.T) {
+		ie := NewUserLocationInformationWithRAI("123", "45", 111, 222)
+
+		rac := ie.MustRAC()
+		if rac != 222 {
+			t.Errorf("wrong rac, got %v", rac)
+		}
+
+		lac := ie.MustLAC()
+		if lac != 111 {
+			t.Errorf("wrong lac, got %v", lac)
+		}
+
+		mcc := ie.MustMCC()
+		if mcc != "123" {
+			t.Errorf("wrong mcc, got %v", mcc)
+		}
+
+		mnc := ie.MustMNC()
+		if mnc != "45" {
+			t.Errorf("wrong mnc, got %v", mnc)
+		}
+	})
+}
+
+func TestUserLocationInformationWithSAI(t *testing.T) {
+	t.Run("Test UserLocationInformation with SAI", func(t *testing.T) {
+		ie := NewUserLocationInformationWithSAI("123", "45", 111, 222)
+
+		sac := ie.MustSAC()
+		if sac != 222 {
+			t.Errorf("wrong sac, got %v", sac)
+		}
+
+		lac := ie.MustLAC()
+		if lac != 111 {
+			t.Errorf("wrong lac, got %v", lac)
+		}
+
+		mcc := ie.MustMCC()
+		if mcc != "123" {
+			t.Errorf("wrong mcc, got %v", mcc)
+		}
+
+		mnc := ie.MustMNC()
+		if mnc != "45" {
+			t.Errorf("wrong mnc, got %v", mnc)
+		}
+	})
+}
